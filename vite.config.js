78d22476c4c2a78ad018ce3392428564aab764d6
Amir Reza Asadi <asadiaa@mail.uc.edu>
2024-11-26 16:07:57 -0500

    fix config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import * as path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import path from 'node:path'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
   plugins: [react()],
